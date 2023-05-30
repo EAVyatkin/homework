1514d5a4d279f94529495fab64500282744ee0b6
EAVyatkin <Vyatkun.e@gmail.com>
2023-05-30 21:19:14 +0500

    add

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -12,10 +12,11 @@ string = 'Hello'
 
 
 def global_changes():
-    # Здесь нужно написать код
+        global number, string
+        number, string = 5, 'Hello, dear friend'
+        return number, string
 
-
-
+global_changes()
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 assert number == 5, 'Переменная number должна иметь значение 5'
 assert string == 'Hello, dear friend', 'Переменная string должна иметь значение Hello, dear friend'
